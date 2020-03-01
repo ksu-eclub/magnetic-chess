8a5ca8a1cbcb62e33c93f6bffacb698876f1bf2c
Zach Deibert <zachdeibert@gmail.com>
2020-03-01 10:49:29 -0600

    Implemented streaming through logic.c

--- logic/rpc/ChessServiceImpl.cpp
+++ logic/rpc/ChessServiceImpl.cpp
@@ -1,5 +1,8 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
 #include <rpc/chess_service.pb.h>
 #include <rpc/chess_service.grpc.pb.h>
 #include "../logic.h"
@@ -7,6 +10,14 @@
 #ifndef NO_GRPC
 
 #define MOVES_BUFFER_SIZE 16
+
+extern "C" void chess_service_impl_listener_proxy(color_t turn, state_t state, char board[64], void *context);
+
+ChessServiceImpl::ChessServiceImpl() {
+    memset(boardBuf, 0, 65);
+    void *tmp;
+    add_listener(chess_service_impl_listener_proxy, this, &tmp);
+}
 
 grpc::Status ChessServiceImpl::ResetBoard(grpc::ServerContext *context, const google::protobuf::Empty *request, google::protobuf::Empty *response) {
     reset_board();
@@ -53,16 +64,37 @@ grpc::Status ChessServiceImpl::RestorePiece(grpc::ServerContext *context, const 
 }
 
 grpc::Status ChessServiceImpl::StreamBoard(grpc::ServerContext *context, const google::protobuf::Empty *request, grpc::ServerWriter<magnetic_chess::GameState> *writer) {
-    puts("StreamBoard");
-    while (!context->IsCancelled()) {
-        magnetic_chess::GameState state;
-        state.set_state(magnetic_chess::GameState_State::GameState_State_UNKNOWN);
-        state.set_board("abc123");
-        writer->Write(state);
-        sleep(1);
+    std::cout << "Starting board streaming" << std::endl;
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        writer->Write(gameState);
     }
-    puts("Stream Closed");
+    std::chrono::system_clock::time_point deadline = context->deadline();
+    while (!context->IsCancelled()) {
+        std::unique_lock<std::mutex> lck(mtx);
+        if (cv.wait_until(lck, deadline) == std::cv_status::timeout) {
+            break;
+        }
+        writer->Write(gameState);
+    }
+    std::cout << "Board stream closed" << std::endl;
     return grpc::Status::OK;
+}
+
+void ChessServiceImpl::OnBoardChange(color_t turn, state_t state, char board[64]) {
+    std::unique_lock<std::mutex> lck(mtx);
+    gameState.set_state(magnetic_chess::GameState_State::GameState_State_UNKNOWN);
+    if (turn != cr_none && state != st_unknown) {
+        gameState.set_state((magnetic_chess::GameState_State) (state + turn - 1));
+    }
+    memcpy(boardBuf, board, 64);
+    gameState.set_board(boardBuf);
+    cv.notify_all();
+}
+
+void chess_service_impl_listener_proxy(color_t turn, state_t state, char board[64], void *context) {
+    ChessServiceImpl *service = (ChessServiceImpl *) context;
+    service->OnBoardChange(turn, state, board);
 }
 
 #endif
--- logic/rpc/ChessServiceImpl.hpp
+++ logic/rpc/ChessServiceImpl.hpp
@@ -5,13 +5,25 @@
 #include <rpc/chess_service.grpc.pb.h>
 #ifndef NO_GRPC
 
+#include <mutex>
+#include <condition_variable>
+#include "../logic.h"
+
 class ChessServiceImpl final : public magnetic_chess::ChessService::Service {
     public:
+        ChessServiceImpl();
         virtual grpc::Status ResetBoard(grpc::ServerContext *context, const google::protobuf::Empty *request, google::protobuf::Empty *response);
         virtual grpc::Status GetMoves(grpc::ServerContext *context, const magnetic_chess::GetMovesRequest *request, magnetic_chess::GetMovesResponse *response);
         virtual grpc::Status Move(grpc::ServerContext *context, const magnetic_chess::MoveRequest *request, magnetic_chess::MoveResponse *response);
         virtual grpc::Status RestorePiece(grpc::ServerContext *context, const magnetic_chess::RestorePieceRequest *request, magnetic_chess::RestorePieceResponse *response);
         virtual grpc::Status StreamBoard(grpc::ServerContext *context, const google::protobuf::Empty *request, grpc::ServerWriter<magnetic_chess::GameState> *writer);
+        virtual void OnBoardChange(color_t turn, state_t state, char board[64]);
+    
+    private:
+        std::mutex mtx;
+        std::condition_variable cv;
+        magnetic_chess::GameState gameState;
+        char boardBuf[65];
 };
 
 #endif
