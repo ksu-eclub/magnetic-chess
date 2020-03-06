e80c5baddce15216e49b13c6a151d1ab7ce45de5
Zach Deibert <zachdeibert@gmail.com>
2020-03-06 10:56:12 -0600

    Wrote test code for voice recognition

--- voice-control/app/src/main/java/com/github/ksueclub/magneticchess/voicecontrol/MainActivity.kt
+++ voice-control/app/src/main/java/com/github/ksueclub/magneticchess/voicecontrol/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.github.ksueclub.magneticchess.voicecontrol
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+
+private const val SPEECH_REQUEST_CODE = 0
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun testVoiceControl(@Suppress("UNUSED_PARAMETER") view: View?) {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        }
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val str: String? = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.let {
+                it[0]
+            }
+            findViewById<TextView>(R.id.text).apply {
+                text = str
+            }
+            Log.d("MainActivity", str)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
