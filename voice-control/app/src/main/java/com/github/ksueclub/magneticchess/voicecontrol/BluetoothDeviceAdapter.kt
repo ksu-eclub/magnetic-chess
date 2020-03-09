e602eb5d217873d668c19a67dd42ccc9bba52f5f
Zach Deibert <zachdeibert@gmail.com>
2020-03-09 09:44:24 -0500

    Added progress bar to scanning activity

--- voice-control/app/src/main/java/com/github/ksueclub/magneticchess/voicecontrol/BluetoothDeviceAdapter.kt
+++ voice-control/app/src/main/java/com/github/ksueclub/magneticchess/voicecontrol/BluetoothDeviceAdapter.kt
@@ -3,7 +3,9 @@ package com.github.ksueclub.magneticchess.voicecontrol
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.reflect.KFunction1
 
@@ -22,23 +24,33 @@ class BluetoothDeviceAdapter(recycler: RecyclerView, callback: KFunction1<Blueto
         val view = LayoutInflater.from(parent.context).inflate(R.layout.bluetooth_device, parent, false)
         view.setOnClickListener(listener)
         return ViewHolder(view)
-}
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val dev = list[position]
         holder.view.apply {
-            if (dev.deviceName == null) {
-                findViewById<TextView>(R.id.device_name).text = context.getString(R.string.unknown_device_name)
+            if (position < list.size) {
+                val dev = list[position]
+                if (dev.deviceName == null) {
+                    findViewById<TextView>(R.id.device_name).text = context.getString(R.string.unknown_device_name)
+                } else {
+                    findViewById<TextView>(R.id.device_name).text = dev.deviceName
+                }
+                findViewById<TextView>(R.id.mac_address).text = dev.macAddress
+                findViewById<TextView>(R.id.rssi).text = String.format(holder.view.context.getString(R.string.dBm), dev.rssi)
+                findViewById<ProgressBar>(R.id.scan_progress).visibility = View.INVISIBLE
+                findViewById<ConstraintLayout>(R.id.device_layout).background = holder.view.context.getDrawable(R.drawable.border)
             } else {
-                findViewById<TextView>(R.id.device_name).text = dev.deviceName
+                findViewById<TextView>(R.id.device_name).text = ""
+                findViewById<TextView>(R.id.mac_address).text = ""
+                findViewById<TextView>(R.id.rssi).text = ""
+                findViewById<ProgressBar>(R.id.scan_progress).visibility = View.VISIBLE
+                findViewById<ConstraintLayout>(R.id.device_layout).background = null
             }
-            findViewById<TextView>(R.id.mac_address).text = dev.macAddress
-            findViewById<TextView>(R.id.rssi).text = String.format(holder.view.context.getString(R.string.dBm), dev.rssi)
         }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return list.size + 1
     }
 
     fun add(dev: BluetoothDevice) {
